fc14db16f853c0b90d368b19b65f5ab240dc425e
John Blackwell <jblackwell072588@gmail.com>
2025-02-28 11:42:33 -0500

    Update Footer.tsx
    Remove email per client

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -11,11 +11,6 @@ const Footer = () => {
             <Phone /> 804-307-4018
           </a>
         </li>
-        <li>
-          <a href="mailto:black.jackb44@gmail.com">
-            <Email /> black.jackb44@gmail.com
-          </a>
-        </li>
       </ul>
       <small>&copy; {new Date().getFullYear()} Gourmet Getcha There</small>
     </Container>
